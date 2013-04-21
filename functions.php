1db1656999ce82d337d72e77770909f120b43624
Neil E. Pearson <neil@helium.net.au>
2013-04-21 19:36:04 +1000

    added latest tweet (back end only)

--- functions.php
+++ functions.php
@@ -25,16 +25,67 @@ class Neil2013 {
             'default'   => null
         ));
 
+        $wpc->add_setting('n13_twitter_screen_name', array(
+            'default'   => null
+        ));
+
         $wpc->add_control(new WP_Customize_Image_Control($wpc, 'n13_footer_image', array(
             'label'     => 'Footer Image',
             'section'   => 'n13_footer_section'
 
         )));
 
+        $wpc->add_control('n13_twitter_screen_name', array(
+            'label'     => 'Twitter ID',
+            'section'   => 'n13_footer_section'
+        ));
+
     }
 
     public static function excerptMore() {
         return '&hellip;';
+    }
+
+    public static function latestTweet() {
+
+        if(class_exists('SimpleXMLElement') && $screenName = get_theme_mod('n13_twitter_screen_name')) {
+
+            $screenName = str_replace('@', '', $screenName);
+
+            $tweet = get_option('n13_tweet');
+
+            if(!$tweet || $tweet->screenName !== $screenName) {
+                $tweet = (object) array(
+                    'refreshed' => 0,
+                    'screenName' => $screenName
+                );
+            }
+
+            if($tweet->refreshed < time() - 60 * 3) {
+
+                $rawXml = file_get_contents("http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=$screenName");
+
+                if($rawXml) {
+
+                    $xml = new SimpleXMLElement($rawXml);
+                    $item = $xml->channel->item[0];
+
+                    $tweet->description = preg_replace('`^.*?:\s+`', '', $item->description);
+                    $tweet->pubDate = strtotime($item->pubDate);
+                    $tweet->link = (string) $item->link;
+
+                    $tweet->refreshed = time();
+
+                    update_option('n13_tweet', $tweet);
+                }
+            }
+
+            if($tweet->pubDate) {
+                return $tweet;
+            }
+        }
+
+        return false;
     }
 
 }
